fc3ae05510cf2c0118a9a5550935a617fc7e7f37
BanditoHayden <haydenjamesmckean@gmail.com>
2023-08-14 18:51:57 -0700

    Spiderman Box

--- Items/Vanity/Spiderman/ArachnoidSuit.cs
+++ Items/Vanity/Spiderman/ArachnoidSuit.cs
@@ -6,10 +6,35 @@ using Microsoft.Xna.Framework.Graphics;
 using System;
 using Terraria.DataStructures;
 using Terraria.GameContent;
+using Conquest.Items.Vanity.DevCoat;
 
 namespace Conquest.Items.Vanity.Spiderman
 {
-	[AutoloadEquip(EquipType.Head)]
+    public class ArachnoidLunchbox : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.maxStack = 9999;
+            Item.consumable = true;
+            Item.width = 32;
+            Item.height = 24;
+            Item.rare = ItemRarityID.Pink;
+        }
+
+        public override bool CanRightClick()
+        {
+            return true;
+        }
+
+        public override void RightClick(Player player)
+        {
+            player.QuickSpawnItem(null, ModContent.ItemType<ArachnoidSuit>());
+            player.QuickSpawnItem(null, ModContent.ItemType<ArachnoidMask>());
+            player.QuickSpawnItem(null, ModContent.ItemType<ArachnoidPants>());
+        }
+    }
+    [AutoloadEquip(EquipType.Head)]
+
 	public class ArachnoidMask : ModItem
 	{
 		public override void SetDefaults() {
